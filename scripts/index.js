0212fc61332fd042d9b735432d44dba0a9d9d2b0
mikecal28 <mt28.caldwell@gmail.com>
2022-05-25 14:26:10 -0600

    "adds image swipe functionality"

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-console.log("Hello Script");
+const slider = document.getElementById("slider");
+
+slider.addEventListener("input", (e) => {
+  const sliderValue = e.target.value;
+  const img = document.querySelector(".foreground-img");
+
+  img.style.width = `${sliderValue}%`;
+});
+
+// slider.addEventListener("click", () => console.log("click"));
